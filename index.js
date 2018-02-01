d88fdeeefdd6032fb2c32f4775ec0b74745af896
nisaruj <nisaruj@hotmail.com>
2018-02-01 15:44:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var express = require('express');
