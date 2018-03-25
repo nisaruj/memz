bd78c2aa7bfcbc84c0f98b3338fa9c012c3f8863
Nisaruj <nisaruj@hotmail.com>
2018-03-25 20:54:29 +0700

    Update admin panel

--- index.js
+++ index.js
@@ -63,7 +63,9 @@ app.post('/admin',function(req,res){
     Lesson.find({},function(err,lesson_res){
         for (var i=0;i<lesson_res.length;i++) {
             lesson_res[i].avail = is_avail.has(i);
-            Lesson.update({_id: lesson_res[i]._id}, {$set: {avail: lesson_res[i].avail}});
+            Lesson.update({_id: lesson_res[i]._id}, {avail: lesson_res[i].avail},function(err,aff) {
+                console.log('Updated %d',aff);
+            });
         }
         console.log(lesson_res);
         res.render('admin',{_lesson:lesson_res});
